da8be05b38bc3671ec5a315b5088cbb26fee484c
Vyan Ary Pratama <vyanaryprabowo9763@gmail.com>
2018-04-16 22:05:07 +0700

    no message

---
+++ app/src/main/java/com/pratamatechnocraft/e_arsip/DashboardFragment.java
@@ -0,0 +1,25 @@
+package com.pratamatechnocraft.e_arsip;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class DashboardFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.activity_dashboard_fragment, container, false);
+    }
+
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        //you can set the title for your toolbar here for different fragments different titles
+        getActivity().setTitle("E-Arsip | Dashboard");
+    }
+}
