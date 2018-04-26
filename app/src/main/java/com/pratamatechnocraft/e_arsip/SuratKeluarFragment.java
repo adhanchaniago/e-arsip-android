e66e9cb9cd5f74e9fbcb5ed97e5e54d1909ded77
Vyan Ary Pratama <vyanaryprabowo9763@gmail.com>
2018-04-26 10:51:47 +0700

    no message

---
+++ app/src/main/java/com/pratamatechnocraft/e_arsip/SuratKeluarFragment.java
@@ -0,0 +1,26 @@
+package com.pratamatechnocraft.e_arsip;
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SuratKeluarFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.activity_surat_keluar_fragment, container, false);
+    }
+
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        //you can set the title for your toolbar here for different fragments different titles
+        getActivity().setTitle("E-Arsip | Surat Keluar");
+    }
+}
