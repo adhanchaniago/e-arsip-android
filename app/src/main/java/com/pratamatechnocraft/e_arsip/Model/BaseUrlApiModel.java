c119ed5462e8129e263a7ae0f3f1850cf4405d3d
Vyan Ary Pratama <vyanaryprabowo9763@gmail.com>
2018-05-29 11:12:29 +0700

    no message

--- app/src/main/java/com/pratamatechnocraft/e_arsip/Model/BaseUrlApiModel.java
+++ app/src/main/java/com/pratamatechnocraft/e_arsip/Model/BaseUrlApiModel.java
@@ -1,7 +1,7 @@
 package com.pratamatechnocraft.e_arsip.Model;
 
 public class BaseUrlApiModel {
-    private String baseURL="http://192.168.1.21/proyek/e-surat/";
+    private String baseURL="http://e-arsip.pratamatechnocraft.com/";
 
     public String getBaseURL() {
         return baseURL;
